3f2f6ca3a6720f224349508499a672d8b319d434
Justin Watt <justincwatt@gmail.com>
2012-02-08 19:59:50 -0800

    add title_to_blogger_style_permalink fallback algorithm for WXR imports

--- wp-maintain-blogger-permalinks.php
+++ wp-maintain-blogger-permalinks.php
@@ -1,11 +1,14 @@
 <?php
 /*
 Plugin Name: Maintain Blogger Permalinks
-Version: 1.1
+Version: 2.0
 Plugin URI: http://justinsomnia.org/2006/10/maintain-permalinks-moving-from-blogger-to-wordpress/
-Description: Update your newly imported Blogger posts with their old Blogger generated URL "slugs"
+Description: Update your newly imported Blogger posts with their old Blogger generated URL "slugs". This is a utility plugin that only needs to be run once. After than you can deactivate and delete it.
 Author: Justin Watt
 Author URI: http://justinsomnia.org/
+
+2.0
+Add fallback algorithm to generate Blogger-link permalink in the absense of an import-derived meta_key
 
 1.1
 Improved success and failure output
@@ -17,7 +20,7 @@ initial version
 LICENSE
 
 wp-maintain-blogger-permalinks.php
-Copyright (C) 2009 Justin Watt
+Copyright (C) 2012 Justin Watt
 justincwatt@gmail.com
 http://justinsomnia.org/
 
@@ -47,35 +50,43 @@ function maintain_blogger_permalinks()
   global $wpdb;
 
   print "<div class='wrap'>";
-  print "<h2>Maintain Blogger Permalinks</h2>";
-
+  print "<div class='icon32' id='icon-tools'><br/></div><h2>Maintain Blogger Permalinks</h2>";
 
   if (isset($_POST['function'])) {
-    print "<p><strong>Progress:</strong></p>";
+    $records = $wpdb->get_results("select ID, post_title, post_name from $wpdb->posts where post_type='post' and post_status = 'publish'");
 
-    $meta_records = $wpdb->get_results("select * from $wpdb->postmeta where meta_key = 'blogger_permalink'");
-    
-    if (count($meta_records) == 0) {
-      
-      print "Sorry I couldn't find any blogger_permalink custom fields, did you run <a href='import.php'>the Blogger importer</a> yet?";
+    if (count($records) == 0) {
+      print "Sorry I couldn't find any posts to fix";
 
     } else {
       $results = '';
-      foreach ($meta_records as $meta_record) {
-        $blogger_permalink = $meta_record->meta_value;
-        $matches = array();
-        if (preg_match('#/[0-9]{4}/[0-9]{2}/(.*?)\.html$#', trim($blogger_permalink), $matches)) {
-          $blogger_permalink = $matches[1];
-          $sql = "update $wpdb->posts set post_name = '$blogger_permalink' WHERE ID = '$meta_record->post_id';";
-          $results .= $sql . "\n";
-          $wpdb->query($sql);
+      foreach ($records as $record) {
+        
+        // first, lets look for a blogger_permalink meta_key for this post
+        $meta_records = $wpdb->get_results("select * from $wpdb->postmeta where meta_key = 'blogger_permalink' and post_id = $record->ID");
+        if (count($meta_records) == 1) {
+          $matches = array();
+          if (preg_match('#/[0-9]{4}/[0-9]{2}/(.*?)\.html$#', trim($meta_records[0]->meta_value), $matches)) {
+            $blogger_permalink = $matches[1];
+          }
+        
+        // if we didn't find a blogger_permalink meta_key, fallback on our best-effort algorithm
+        } else {
+          $blogger_permalink = title_to_blogger_style_permalink($record->post_title);
         }
-      }
-      print "Done!<br/>";
-
-      print "<textarea rows='10' cols='80' wrap='off' readonly='readonly'>";
-      print htmlspecialchars($results);
-      print "</textarea>";
+        
+        if ($record->post_name != $blogger_permalink && $blogger_permalink != '') {
+        
+          $sql = "update $wpdb->posts set post_name = '$blogger_permalink' WHERE ID = '$record->ID';";
+          $wpdb->query($sql); 
+          $results .= "Updated \"$record->post_name\" to \"$blogger_permalink\"<br/>";
+        }       
+      }      
+      
+      print "<strong>Done!</strong> This is what happened:<br/>";
+      //print "<textarea rows='10' cols='80' wrap='off' readonly='readonly'>";
+      print $results;
+      //print "</textarea>";
     }
 
   
@@ -84,7 +95,7 @@ function maintain_blogger_permalinks()
     ?>
     
     <form action='' method='post'>
-    <input type='submit' name='function' value='Maintain Blogger Permalinks' />
+    <input type='submit' class='button' name='function' value='Maintain Blogger Permalinks' />
     </form>
 
     <?php
@@ -92,6 +103,83 @@ function maintain_blogger_permalinks()
   }
 
   print "</div>";
+}
+
+
+function title_to_blogger_style_permalink($title) 
+{
+  $blogger_permalink = $title;
+  
+  //strip potential entities (overzealously)
+  $blogger_permalink = preg_replace("/&[^ ]+/", "", $blogger_permalink);
+  
+  //replace potential tags with a space
+  $blogger_permalink = preg_replace("/<\/?[^>]>/", " ", $blogger_permalink);
+  
+  //replace dashes with spaces
+  $blogger_permalink = str_replace("-", " ", $blogger_permalink);
+  
+  //replace latin characters with ascii equivalents
+  //technically this should be date dependent, older versions of Blogger stripped these characters
+  $blogger_permalink = remove_accents($blogger_permalink);
+
+  //strip out non a-z, 0-9 (over simplification, they latinize accented chars now)
+  $blogger_permalink = preg_replace("/[^a-zA-Z0-9 ]/", "", $blogger_permalink);
+
+  //lowercase everything
+  $blogger_permalink = strtolower($blogger_permalink);
+
+  //compress whitespace
+  $blogger_permalink = preg_replace("/\s+/", " ", $blogger_permalink);
+
+  //trim
+  $blogger_permalink = trim($blogger_permalink);
+  
+  //split into words
+  $blogger_permalink = explode(" ", $blogger_permalink);
+
+  //remove a, an, the
+  $temp = array();
+  foreach ($blogger_permalink as $key => $value) {
+    if ($value != 'the' && $value != 'a' && $value != 'an') {
+      $temp[] = $value;
+    }
+  }
+  $blogger_permalink = $temp;
+
+  //truncate before word that crosses boundary of 39th character!
+  $character_count = 0;
+  foreach ($blogger_permalink as $key => $value) {
+    
+    $word_length = strlen($value);
+    
+    //if first word is longer than 39 characters, truncate
+    if ($key == 0 && $word_length > 39) {
+        $word = substr($value, 0, 39);
+        $blogger_permalink = array($word);
+        break;
+    }   
+
+    $character_count += $word_length;
+    
+    if ($character_count < 39) {
+      $character_count++; // add count for space
+    } elseif ($character_count == 39) {
+      $blogger_permalink = array_slice($blogger_permalink, 0, $key+1);
+      break;
+    } else { // we've gone over
+      $blogger_permalink = array_slice($blogger_permalink, 0, $key);
+      break;
+    }
+  }
+  
+  // put the title back together
+  $blogger_permalink = implode(" ", $blogger_permalink);
+
+  //replace spaces with dashes
+  $blogger_permalink = str_replace(" ", "-", $blogger_permalink);
+
+  return $blogger_permalink;
 }
 
 
